89a8bc44186fd5398bfacf5fbb44b8726976d3af
DManstrator <DManstrator@gmail.com>
2023-06-24 22:20:20 +0200

    Trying to print all Mixers again

--- src/main/java/tk/dmanstrator/audioplayer/DAudioPlayer.java
+++ src/main/java/tk/dmanstrator/audioplayer/DAudioPlayer.java
@@ -168,6 +168,10 @@ public class DAudioPlayer {
         DataLine.Info info = new DataLine.Info(Clip.class, audioFormat, bufferSize);
 
         try {
+            List<?> providers = JDK13Services.getProviders(MixerProvider.class);
+            System.out.println("Available Mixers: " + providers.size());
+            System.out.println(providers);
+
             Info[] sourceLineInfo = AudioSystem.getSourceLineInfo(info);
             System.out.println("Source Line Info Objects: " + sourceLineInfo.length);
             for (Info i : sourceLineInfo)  {
